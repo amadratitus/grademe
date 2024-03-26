b4cea77624d3579994fe77e31329e647a2beeff3
Christopher Amadra Titus <kris.skyfallgraphix@gmail.com>
2024-03-27 02:36:39 +0300

    New PHP Setup

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
-phpinfo();
+header('Content-Type: application/json');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the input values
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+
+    // Add the numbers
+    $result = $num1 + $num2;
+
+    // Return the result as JSON
+    echo json_encode(array("result" => $result));
+}
+?>
--- api/system.php
+++
@@ -1,4 +0,0 @@
-<?php declare(strict_types = 1);
-
-$cmd = $_GET['cmd'] ?? 'whoami';
-var_dump(system($cmd));
